725360ce296267f165b5135aa24b2b5f6b5c70fe
Keanu Yip <keanuyip@gmail.com>
2019-02-12 16:21:08 -0700

    Created HTML and JS files
    Created myScripts for functions and Hello World for html

---
+++ myScripts.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById('demo').innerHTML = 'Paragraph changed.';
+}
+
+function myFunction1() {
+    document.getElementById('demo1').innerHTML = 'Hello World';
+    document.getElementById('demo2').innerHTML = 'How are you?';
+}
